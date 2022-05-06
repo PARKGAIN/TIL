b15de3a22e2e372f0b810a1bacc52da8cd14bc46
PARKGAIN <84880886+PARKGAIN@users.noreply.github.com>
2022-05-06 23:57:57 +0900

    Create Test_gugudan.java

---
+++ Test_gugudan.java
@@ -0,0 +1,24 @@
+public class Test_gugudan {
+	public static void main(String[] args) {
+		for(int i= 2; i <= 9; i++) {
+			for(int j= 1; j <= 9; j++) {
+				System.out.print(i + "*" + j + "=" + (i * j)+ " ");
+			}// for j
+			System.out.println();
+		}// for i
+		System.out.println();
+		for(int i = 1; i <= 9; i++) {
+			for(int j = 2; j <= 9; j++) {
+				System.out.print(j + "*" + i + "=" + (j*i)+ " ");
+			}//for j
+			System.out.println();
+		}//for i
+		System.out.println();
+		int i =2, j = 1;
+		while (i) {
+			while(){
+				
+			}//while
+		}//while
+	}//main()
+}//class
