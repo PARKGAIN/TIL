05fe829c921398a324852e6aca61dfac519f11cf
PARKGAIN <84880886+PARKGAIN@users.noreply.github.com>
2022-05-20 13:11:10 +0900

    Create FruitDAO.java

---
+++ FruitDAO.java
@@ -0,0 +1,31 @@
+package com.hanul.fruit;
+
+public class FruitDAO {
+	//디포트 생성자 메소드 : 생성자 메소드가 없을 경우 JVM 자동제공
+	public FruitDAO() {}
+	
+	private FruitDTO[] fruit; //멤버변수로 만들어줌
+	public FruitDAO(FruitDTO[] fruit) {
+		this.fruit = fruit;
+	}
+	
+	//가격계산
+	public void getPrice() {
+		
+		
+	}//getPrice()
+	
+	//가격 내림차순 정렬
+	public void PriceDescSort() {
+		
+		
+	}//PriceDescSort()
+	
+	//출력
+	public void display() {
+		
+		
+	}//display()
+	
+	
+}//class
