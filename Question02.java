023286d3b0c60d56f10a7f37f67bd673d55c213a
PARKGAIN <84880886+PARKGAIN@users.noreply.github.com>
2022-05-26 17:35:08 +0900

    2번문제

---
+++ Question02.java
@@ -0,0 +1,13 @@
+public class Question02 {
+	public Question02() {
+		double radius = 5;
+	}
+	
+	public static double getRadius(double radius) {
+		return radius;
+	}//getRadius()
+	
+	public static double getArea(double radius) {
+		return radius*radius;
+	}//getArea()
+}
