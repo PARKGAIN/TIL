f6f509173f2cb385e3944fdf385325264a49f4d6
PARKGAIN <84880886+PARKGAIN@users.noreply.github.com>
2022-05-26 15:10:01 +0900

    6번문제

---
+++ Question06Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Question06Main {
+	public static void main(String[] args) {
+		Scanner scanner =new Scanner(System.in);
+		System.out.println("문자열을 입력해주세요 :");
+		String str = scanner.nextLine();
+		
+		String[] arr =str.split("");
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			
+		}//for
+		
+	}//main()
+}//class
