fff9bb9effc5d7e8ca4bf6e8dac3c935dbb1d2eb
PARKGAIN <84880886+PARKGAIN@users.noreply.github.com>
2022-05-26 12:52:11 +0900

    3번문제

---
+++ Question03Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Question03Main {
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println("반지름을 입력해주세요");
+	
+	int x = Integer.parseInt(scanner.nextLine()); 
+	int y = Integer.parseInt(scanner.nextLine()); 
+}
