9ac092c5580250ad66d8d0b6bff5504454e65635
PARKGAIN <84880886+PARKGAIN@users.noreply.github.com>
2022-05-26 12:51:32 +0900

    1번문제

---
+++ Question01Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Question01Main {
+	public static void main(String[] args) {
+		Question01 q = new Question01();
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("반지름을 입력해주세요");
+		double radius = Double.parseDouble(scanner.nextLine());
+		q.getRadius(radius);
+		System.out.println("반지름 : "+  q.getRadius(radius));
+		q.getArea(radius);
+		System.out.println("원의 넓이 : " + q.getArea(radius));
+		
+		System.out.println("Question02");
+		q.getRadius(5);
+		System.out.println("반지름 : "+  q.getRadius(5));
+		q.getArea(5);
+		System.out.println("원의 넓이 : " + q.getArea(5));
+	}//main()
+}//class
