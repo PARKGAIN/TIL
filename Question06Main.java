2da5b149db0a03adc831852ce99685d3f0becf35
PARKGAIN <84880886+PARKGAIN@users.noreply.github.com>
2022-05-26 15:39:15 +0900

    6번문제

--- Question06Main.java
+++ Question06Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question06Main {
@@ -9,8 +10,9 @@ public class Question06Main {
 		String[] arr =str.split("");
 		int sum = 0;
 		for (int i = 0; i < arr.length; i++) {
-			
+		int arr2 =Integer.parseInt(arr[i]);
+		sum += arr2;
 		}//for
-		
+		System.out.println(sum);
 	}//main()
 }//class
