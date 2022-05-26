376d41a71c22a2de384589ed15f8eb4b12316429
PARKGAIN <84880886+PARKGAIN@users.noreply.github.com>
2022-05-26 13:11:24 +0900

    3번문제

--- Question03Main.java
+++ Question03Main.java
@@ -1,10 +1,31 @@
 import java.util.Scanner;
-
 public class Question03Main {
-	Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("첫번째 정수를 입력하세요 :");
+		int x = Integer.parseInt(scanner.nextLine()); 
+		System.out.println("두번째 정수를 입력하세요 :");
+		int y = Integer.parseInt(scanner.nextLine()); 
+		
+		System.out.println(add(x,y));
+		System.out.println(sub(x,y));
+		System.out.println(mul(x,y));
+		System.out.println(div(x,y));
+		
+	}//main()
 	
-	System.out.println("반지름을 입력해주세요");
+	public static int add(int x,int y) {
+		return x+y;
+	}//add()
+	public static int sub(int x,int y) {
+		return x-y;	//결과가 음수?
+	}//sub()
 	
-	int x = Integer.parseInt(scanner.nextLine()); 
-	int y = Integer.parseInt(scanner.nextLine()); 
+	public static int mul(int x, int y) {
+		return x*y;
+	}//mul()
+	
+	public static int div(int x, int y) {
+		return x/y;
+	}
 }
